f252ebe938014728d5ade3bc0ac5ccd15e56f00c
kylerfoulks <kylerfoulks@gmail.com>
2016-01-23 11:45:56 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-xs-12 headerLoggedIn"></div>
+    	</div>
+    	<div class="row">
+    		<div class="col-xs-4 col-md-2 userNav">
+    			<ul class="col-xs-12">
+    				<li class="active"><span class="glyphicon glyphicon-dashboard"></span><a href="#">Dashboard</a></li>
+    				<li><span class="glyphicon glyphicon-user"></span><a href="#">Clients</a></li>
+    				<li><span class="glyphicon glyphicon-tasks"></span><a href="#">Projects</a></li>
+    				<li><span class="glyphicon glyphicon-usd"></span><a href="#">Billing</a></li>
+    				<li><span class="glyphicon glyphicon-list-alt"></span><a href="#">Reports</a></li>
+    				<li><a href="#">Employees</a></li>
+    			</ul>
+    		</div>
+
+            <div class="col-md-8 content-wrap">
+                <div class="page-title">Dashboard</div>
+            </div>
+
+
+    	</div>
+
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
