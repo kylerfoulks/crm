525c82d1a37746d9fe67ffebc0f9d291c6db0844
kylerfoulks <kylerfoulks@gmail.com>
2016-01-23 18:15:46 -0500

    Change from github

---
+++ employees.php
@@ -0,0 +1,21 @@
+<?php require_once('header.php');?>
+
+
+
+
+    	<div class="row">
+    		<?php require_once('userNav.php');?> 
+
+            <div class="col-md-8 content-wrap">
+                <div class="page-title">Dashboard</div>
+            </div>
+
+
+    	</div>
+
+</div><!-- /Usernav -->
+
+
+
+
+<?php require_once('footer.php');?>
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php require_once('header.php');?>
+<?php require_once('header.php');
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+?>
 
 
 
@@ -9,6 +14,20 @@
             <div class="col-md-8 content-wrap">
                 <div class="page-title">Dashboard</div>
             </div>
+
+
+<?php
+$dsn = "pgsql:"
+    . "host=ec2-54-83-59-203.compute-1.amazonaws.com;"
+    . "dbname=ddrn1hhgb465rs;"
+    . "user=apduhqpapfemah;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=rF0iut51FBnE5UcSFFOjnPqGNs";
+
+if ($db = new PDO($dsn)) {
+    echo 'yes';
+}?>
 
 
     	</div>
