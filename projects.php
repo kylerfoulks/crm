62bf2f3bca4ec97d913c59408dc6786d66cd28ec
kylerfoulks <kylerfoulks@gmail.com>
2016-01-23 22:20:50 -0500

    Theming
    Working on projects page

--- projects.php
+++ projects.php
@@ -7,7 +7,78 @@
     		<?php require_once('userNav.php');?> 
 
             <div class="col-md-8 content-wrap">
-                <div class="page-title">Projects</div>
+                <div class="page-title">Projects</div><a class="newBtn btn btn-primary btn-xs" href="#">New</a>
+
+                <div class="projectPreview col-xs-12">
+                	<div class="top">
+                		<div class="logo"></div><span class="companyTitle">Google, Inc</span><span class="currentPhase">Current Phase: Design</span>
+                		<span class="contactInfo"><strong>Kyle Foulks</strong><br>Head of Marketing<br>(865-587-8778)</span>
+                	</div>
+                	<div class="bottom">
+                		<div class="left">
+                			<a class="tag btn btn-primary btn-xs" href="#">Web</a>
+                			<a class="tag btn btn-primary btn-xs" href="#">Design</a>
+                			<a class="tag btn btn-primary btn-xs" href="#">Marketing</a>
+                		</div>
+                		<div class="right">
+                			Launch Date: 10/15/2016
+                		</div>
+                	</div>
+                </div>
+
+                <div class="projectPreview col-xs-12">
+                	<div class="top">
+                		<div class="logo"></div><span class="companyTitle">Google, Inc</span><span class="currentPhase">Current Phase: Design</span>
+                		<span class="contactInfo"><strong>Kyle Foulks</strong><br>Head of Marketing<br>(865-587-8778)</span>
+                	</div>
+                	<div class="bottom">
+                		<div class="left">
+                			<a class="tag btn btn-primary btn-xs" href="#">Web</a>
+                			<a class="tag btn btn-primary btn-xs" href="#">Design</a>
+                			<a class="tag btn btn-primary btn-xs" href="#">Marketing</a>
+                		</div>
+                		<div class="right">
+                			Launch Date: 10/15/2016
+                		</div>
+                	</div>
+                </div>
+
+                <div class="projectPreview col-xs-12">
+                	<div class="top">
+                		<div class="logo"></div><span class="companyTitle">Google, Inc</span><span class="currentPhase">Current Phase: Design</span>
+                		<span class="contactInfo"><strong>Kyle Foulks</strong><br>Head of Marketing<br>(865-587-8778)</span>
+                	</div>
+                	<div class="bottom">
+                		<div class="left">
+                			<a class="tag btn btn-primary btn-xs" href="#">Web</a>
+                			<a class="tag btn btn-primary btn-xs" href="#">Design</a>
+                			<a class="tag btn btn-primary btn-xs" href="#">Marketing</a>
+                		</div>
+                		<div class="right">
+                			Launch Date: 10/15/2016
+                		</div>
+                	</div>
+                </div>
+
+                <div class="projectPreview col-xs-12">
+                	<div class="top">
+                		<div class="logo"></div><span class="companyTitle">Google, Inc</span><span class="currentPhase">Current Phase: Design</span>
+                		<span class="contactInfo"><strong>Kyle Foulks</strong><br>Head of Marketing<br>(865-587-8778)</span>
+                	</div>
+                	<div class="bottom">
+                		<div class="left">
+                			<a class="tag btn btn-primary btn-xs" href="#">Web</a>
+                			<a class="tag btn btn-primary btn-xs" href="#">Design</a>
+                			<a class="tag btn btn-primary btn-xs" href="#">Marketing</a>
+                		</div>
+                		<div class="right">
+                			Launch Date: 10/15/2016
+                		</div>
+                	</div>
+                </div>
+
+
+
             </div>
 
 
--- userNav.php
+++ userNav.php
@@ -1,10 +1,16 @@
 <div class="col-xs-4 col-md-2 userNav">
-    			<ul class="col-xs-12">
-    				<li class="active"><span class="glyphicon glyphicon-dashboard"></span><a href="#">Dashboard</a></li>
-    				<li><span class="glyphicon glyphicon-user"></span><a href="clients.php">Clients</a></li>
-    				<li><span class="glyphicon glyphicon-tasks"></span><a href="projects.php">Projects</a></li>
-    				<li><span class="glyphicon glyphicon-usd"></span><a href="billing.php">Billing</a></li>
-    				<li><span class="glyphicon glyphicon-list-alt"></span><a href="reports.php">Reports</a></li>
-    				<li><a href="employees.php">Employees</a></li>
-    			</ul>
+		<ul class="col-xs-12">
+			<li class="active"><span class="glyphicon glyphicon-dashboard"></span><a href="dashboard.php">Dashboard</a></li>
+			<li><span class="glyphicon glyphicon-user"></span><a href="clients.php">Clients</a></li>
+			<li><span class="glyphicon glyphicon-tasks"></span><a href="projects.php">Projects</a></li>
+			<li><span class="glyphicon glyphicon-usd"></span><a href="billing.php">Billing</a></li>
+			<li><span class="glyphicon glyphicon-list-alt"></span><a href="reports.php">Reports</a></li>
+			<li><a href="employees.php">Employees</a></li>
+		</ul>
+
+		<ul class="col-xs-12 lowerUL">
+			<li><a href="#">Settings</a></li>
+			<li><a href="#">Log Out</a></li>
+
+		</ul>
 </div>
