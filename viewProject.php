fa9161fac49e2564dab42db53b905fae5d379084
kylerfoulks <kylerfoulks@gmail.com>
2016-01-24 10:11:39 -0500

    Theming
    View projects page.

---
+++ viewProject.php
@@ -0,0 +1,98 @@
+
+<?php require_once('header.php');
+?>
+
+
+
+
+        <div class="row">
+            <?php require_once('userNav.php');?> 
+
+            <div class="col-md-8 content-wrap">
+                <div class="page-title">Create New Website</div><div class="projectDomain">www.google.com</div><div class="phase">Design Phase</div>
+
+                <div class="description col-lg-10">
+                    <h4>Description:</h4>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in justo at elit eleifend convallis. Morbi tortor nibh, eleifend in nisl in, accumsan imperdiet purus. Cras id hendrerit tellus, quis vehicula sem. Proin eget libero turpis. Vivamus eget mi ultricies libero bibendum placerat sed ac odio. Curabitur congue vulputate fermentum. Suspendisse auctor dignissim dignissim. Etiam dolor orci, interdum a suscipit a, dignissim quis est. Fusce in pretium libero. Ut vitae justo feugiat, pulvinar turpis id, congue nibh. In suscipit sapien tempus, condimentum eros sit amet, interdum mauris. Morbi sed commodo diam. Phasellus eu nisi convallis, viverra nulla ac, accumsan metus. Nunc a augue vel ipsum eleifend interdum. Aenean pulvinar nulla id iaculis interdum.
+
+Aenean a ipsum sed nisl vestibulum maximus. 
+
+<br><br>Mauris nec quam at tellus tristique tempus. Ut condimentum dictum mi nec consectetur. Vivamus suscipit urna felis, sed accumsan ante fringilla quis. Ut ipsum turpis, auctor a est sed, porta lacinia eros. Curabitur id neque ut urna gravida facilisis. In egestas erat in sapien porta molestie. Sed efficitur magna purus. Aenean non varius augue. Donec scelerisque aliquet tempor. Integer ornare nisi turpis, ut efficitur nulla mattis non. Cras ullamcorper enim vel sem sagittis feugiat. Phasellus justo purus, consectetur at ipsum et, gravida facilisis ex. 
+
+                </div>
+
+                <div class="crecentials col-lg-2">
+                    <h4>Credentials</h4>
+                    <a class="btn btn-success btn-xs" href="#">Go Daddy</a>
+                    <a class="btn btn-success btn-xs" href="#">FTP</a>
+                    <a class="btn btn-success btn-xs" href="#">Dev</a>
+                    <a class="btn btn-success btn-xs" href="#">CPanel</a>
+                </div>
+
+                <div class="active-tasks col-lg-12">
+                    <h4>- Active Tasks</h4>
+                    <div class="task-single">
+                        <div class="clock col-lg-1"></div>
+                        <div class="title col-lg-3">Edit Footer Text</div>
+                        <div class="due col-lg-3">Due: 11/15/2015</div>
+                        <div class="alloc col-lg-2">3:00</div>
+                        <div class="user col-lg-2">:30</div>
+                        <div class="checkBox col-lg-1">ch</div>
+                    </div>
+
+                     <div class="task-single">
+                        <div class="clock col-lg-1"></div>
+                        <div class="title col-lg-3">Edit Footer Text</div>
+                        <div class="due col-lg-3">Due: 11/15/2015</div>
+                        <div class="alloc col-lg-2">3:00</div>
+                        <div class="user col-lg-2">:30</div>
+                        <div class="checkBox col-lg-1">ch</div>
+                    </div>
+
+                     <div class="task-single">
+                        <div class="clock col-lg-1"></div>
+                        <div class="title col-lg-3">Edit Footer Text</div>
+                        <div class="due col-lg-3">Due: 11/15/2015</div>
+                        <div class="alloc col-lg-2">3:00</div>
+                        <div class="user col-lg-2">:30</div>
+                        <div class="checkBox col-lg-1">ch</div>
+                    </div>
+
+                     <div class="task-single">
+                        <div class="clock col-lg-1"></div>
+                        <div class="title col-lg-3">Edit Footer Text</div>
+                        <div class="due col-lg-3">Due: 11/15/2015</div>
+                        <div class="alloc col-lg-2">3:00</div>
+                        <div class="user col-lg-2">:30</div>
+                        <div class="checkBox col-lg-1">ch</div>
+                    </div>
+
+                     <div class="task-single">
+                        <div class="clock col-lg-1"></div>
+                        <div class="title col-lg-3">Edit Footer Text</div>
+                        <div class="due col-lg-3">Due: 11/15/2015</div>
+                        <div class="alloc col-lg-2">3:00</div>
+                        <div class="user col-lg-2">:30</div>
+                        <div class="checkBox col-lg-1">ch</div>
+                    </div>
+                </div>
+
+                <div class="completedTasks col-sm-12">
+                    <h4>+ Completed Tasks</h4>
+
+
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+</div><!-- /Usernav -->
+
+
+
+
+<?php require_once('footer.php');?>
